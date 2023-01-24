f22fc58c899772127080c69fb4335e2ce1acb0e2
altersystem <altersystem.dev@gmail.com>
2023-01-23 23:35:33 -0600

    Product.js terminado

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from 'react';
+import StarIcon from '@mui/icons-material/Star';
+import Currency from 'react-currency-formatter'
 
-function Product({id, title, price, description, category, image}) {
+
+function Product({id, title, price, description, category, image, rating}) {
+
+  const hasPrime = true;
+
   return (
-    <div>
-        <p>{category}</p>
-        <img src={image} alt="title" width={200} height={200} objectFit="contain" />
+    <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
+        <p className='absolute top-2 right-2 text-xs italic text-gray-400'>{category}</p>
+        <img className='mx-auto' src={image} alt="title" width={200} height={200} objectFit="contain" />
+        <h4 className='mt-4' >{title}</h4>
+
+        <div className='flex'>
+          {
+            Array(Math.round(rating.rate))
+            .fill()
+            .map((_, i) => (
+              <StarIcon className='h-5 text-yellow-600' />
+            ))
+          }
+        </div>
+
+        <p className='text-xs my-2 line-clamp'>{description}</p>
+
+        <div className='mb-5'>
+          <Currency quantity={price} currency="CRC"/>
+        </div>
+
+        {hasPrime && (
+          <div className='flex items-center space-x-2 -mt-5'>
+            <img className='w-12' src='https://links.papareact.com/fdw' alt='logo prime' />
+            <p className='text-xs text-gray-500'>Free next-day Delivery</p>
+          </div>
+        )}
+
+        <button className='mt-auto button'>Add to Basket</button>
+
     </div>
   )
 }
 
-export default Product
+export default Product;
--- src/components/productFeed/ProductFeed.js
+++ src/components/productFeed/ProductFeed.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import {staticData} from '../../data';
-import Product from '../Product/Product';
-
-
+import React from "react";
+import { staticData } from "../../data";
+import Product from "../Product/Product";
 
 function ProductFeed() {
   const products = staticData;
   return (
-    <div>
-       <p>ProductFeed</p>
-       {products.map((product) => (
-          <Product 
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products.slice(0,4).map((product) => (
+        <Product
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+          category={product.category}
+          image={product.image}
+          rating={product.rating}
+        />
+      ))}
+
+      <img
+        className="md:col-span-full"
+        src="https://links.papareact.com/dyz"
+        alt="anuncio"
+      />
+
+      <div className="md:col-span-2 ">
+        {products.slice(4,5).map((product) => (
+          <Product
             key={product.id}
             id={product.id}
             title={product.title}
@@ -20,10 +37,23 @@ function ProductFeed() {
             image={product.image}
             rating={product.rating}
           />
-       ))}
-       
+        ))}
+      </div>
+
+      {products.slice(5,products.length).map((product) => (
+        <Product
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+          category={product.category}
+          image={product.image}
+          rating={product.rating}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default ProductFeed
+export default ProductFeed;
